4ad1b67bd87c9d5e9c29d08b93508069fb31a1e5
Vitor <vitordacruz@gmail.com>
2023-10-14 19:25:39 -0300

    Adicionado alteração do usuário

--- src/main/java/com/ead/authuser/controllers/UserController.java
+++ src/main/java/com/ead/authuser/controllers/UserController.java
@@ -1,12 +1,16 @@
 package com.ead.authuser.controllers;
 
+import com.ead.authuser.dtos.UserDTO;
 import com.ead.authuser.models.UserModel;
 import com.ead.authuser.services.UserService;
+import com.fasterxml.jackson.annotation.JsonView;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -15,6 +19,8 @@ import java.util.UUID;
 @CrossOrigin(origins = "*",maxAge = 3600)
 @RequestMapping("/users")
 public class UserController {
+    public static final String USER_NOT_FOUND = "User not found";
+
     @Autowired
     UserService userService;
 
@@ -25,25 +31,80 @@ public class UserController {
     }
 
     @GetMapping("/{userId}")
-    public ResponseEntity<?> getOneUser(@PathVariable(value = "userId") UUID userId) {
+    public ResponseEntity<Object> getOneUser(@PathVariable(value = "userId") UUID userId) {
         Optional<UserModel> userModelOptional = userService.findById(userId);
         if (!userModelOptional.isPresent()) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                    .body("User not found");
+                    .body(USER_NOT_FOUND);
         } else {
             return ResponseEntity.status(HttpStatus.OK).body(userModelOptional.get());
         }
     }
 
     @DeleteMapping("/{userId}")
-    public ResponseEntity<?> deleteUser(@PathVariable(value = "userId") UUID userId) {
+    public ResponseEntity<Object> deleteUser(@PathVariable(value = "userId") UUID userId) {
         Optional<UserModel> userModelOptional = userService.findById(userId);
         if (!userModelOptional.isPresent()) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                    .body("User not found");
+                    .body(USER_NOT_FOUND);
         } else {
             userService.delete(userModelOptional.get());
             return ResponseEntity.status(HttpStatus.OK).body("User successfully deleted");
         }
     }
+
+    @PutMapping("/{userId}")
+    public ResponseEntity<Object> updateUser(@PathVariable(value = "userId") UUID userId,
+                                        @RequestBody @JsonView(UserDTO.UserView.UserPut.class) UserDTO userDTO) {
+        Optional<UserModel> userModelOptional = userService.findById(userId);
+        if (!userModelOptional.isPresent()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body(USER_NOT_FOUND);
+        } else {
+            var userModel = userModelOptional.get();
+            userModel.setFullName(userDTO.getFullName());
+            userModel.setPhoneNumber(userDTO.getPhoneNumber());
+            userModel.setCpf(userDTO.getCpf());
+            userModel.setLastUpdateDate(LocalDateTime.now(ZoneId.of("UTC")));
+            userService.save(userModel);
+            return ResponseEntity.status(HttpStatus.OK).body(userModel);
+        }
+    }
+
+    @PutMapping("/{userId}/password")
+    public ResponseEntity<String> updatePassword(@PathVariable(value = "userId") UUID userId,
+                                                 @RequestBody @JsonView(UserDTO.UserView.PasswordPut.class) UserDTO userDTO) {
+        Optional<UserModel> userModelOptional = userService.findById(userId);
+        if (!userModelOptional.isPresent()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body(USER_NOT_FOUND);
+        }
+        var userModel = userModelOptional.get();
+        if (!userModel.getPassword().equals(userDTO.getOldPassword())) {
+            return ResponseEntity.status(HttpStatus.CONFLICT)
+                    .body("Error: Mismatched old password");
+        } else {
+            userModel.setPassword(userDTO.getPassword());
+            userModel.setLastUpdateDate(LocalDateTime.now(ZoneId.of("UTC")));
+            userService.save(userModel);
+            return ResponseEntity.status(HttpStatus.OK).body("Password Updated!");
+        }
+    }
+
+    @PutMapping("/{userId}/image")
+    public ResponseEntity<Object> updateImage(@PathVariable(value = "userId") UUID userId,
+                                         @RequestBody @JsonView(UserDTO.UserView.ImagePut.class) UserDTO userDTO) {
+        Optional<UserModel> userModelOptional = userService.findById(userId);
+        if (!userModelOptional.isPresent()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body(USER_NOT_FOUND);
+        } else {
+            var userModel = userModelOptional.get();
+            userModel.setImageUrl(userDTO.getImageUrl());
+            userModel.setLastUpdateDate(LocalDateTime.now(ZoneId.of("UTC")));
+            userService.save(userModel);
+            return ResponseEntity.status(HttpStatus.OK).body(userModel);
+        }
+    }
+
 }
