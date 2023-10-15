9614866b8b6b77a4a4374ba1116c4a4854d4f058
Vitor <vitordacruz@gmail.com>
2023-10-15 18:15:09 -0300

    Adicionado validação no cadastro do usuário

--- src/main/java/com/ead/authuser/controllers/UserController.java
+++ src/main/java/com/ead/authuser/controllers/UserController.java
@@ -7,6 +7,7 @@ import com.fasterxml.jackson.annotation.JsonView;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
@@ -55,7 +56,8 @@ public class UserController {
 
     @PutMapping("/{userId}")
     public ResponseEntity<Object> updateUser(@PathVariable(value = "userId") UUID userId,
-                                        @RequestBody @JsonView(UserDTO.UserView.UserPut.class) UserDTO userDTO) {
+                                        @RequestBody @Validated(UserDTO.UserView.UserPut.class)
+                                        @JsonView(UserDTO.UserView.UserPut.class) UserDTO userDTO) {
         Optional<UserModel> userModelOptional = userService.findById(userId);
         if (!userModelOptional.isPresent()) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND)
@@ -73,7 +75,8 @@ public class UserController {
 
     @PutMapping("/{userId}/password")
     public ResponseEntity<String> updatePassword(@PathVariable(value = "userId") UUID userId,
-                                                 @RequestBody @JsonView(UserDTO.UserView.PasswordPut.class) UserDTO userDTO) {
+                                                 @RequestBody @Validated(UserDTO.UserView.PasswordPut.class)
+                                                 @JsonView(UserDTO.UserView.PasswordPut.class) UserDTO userDTO) {
         Optional<UserModel> userModelOptional = userService.findById(userId);
         if (!userModelOptional.isPresent()) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND)
@@ -93,7 +96,8 @@ public class UserController {
 
     @PutMapping("/{userId}/image")
     public ResponseEntity<Object> updateImage(@PathVariable(value = "userId") UUID userId,
-                                         @RequestBody @JsonView(UserDTO.UserView.ImagePut.class) UserDTO userDTO) {
+                                         @RequestBody @Validated(UserDTO.UserView.ImagePut.class)
+                                         @JsonView(UserDTO.UserView.ImagePut.class) UserDTO userDTO) {
         Optional<UserModel> userModelOptional = userService.findById(userId);
         if (!userModelOptional.isPresent()) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND)
--- src/main/java/com/ead/authuser/dtos/UserDTO.java
+++ src/main/java/com/ead/authuser/dtos/UserDTO.java
@@ -3,6 +3,10 @@ package com.ead.authuser.dtos;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonView;
 import lombok.Data;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.UUID;
 
 @Data
@@ -18,20 +22,36 @@ public class UserDTO {
 
     private UUID userID;
 
+    @NotBlank(groups = UserView.RegistrationPost.class)
+    @Size(min = 4, max = 50, groups = UserView.RegistrationPost.class)
     @JsonView(UserView.RegistrationPost.class)
     private String username;
-    @JsonView(UserView.RegistrationPost.class)
+
+    @NotBlank(groups = {UserView.RegistrationPost.class})
+    @Email
+    @JsonView({UserView.RegistrationPost.class})
     private String email;
+
+    @NotBlank(groups = {UserView.RegistrationPost.class, UserView.PasswordPut.class})
+    @Size(min = 6, max = 20)
     @JsonView({UserView.RegistrationPost.class, UserView.PasswordPut.class})
     private String password;
+
+    @NotBlank(groups = UserView.PasswordPut.class)
+    @Size(min = 6, max = 20)
     @JsonView(UserView.PasswordPut.class)
     private String oldPassword;
+
     @JsonView({UserView.RegistrationPost.class, UserView.UserPut.class})
     private String fullName;
+
     @JsonView({UserView.RegistrationPost.class, UserView.UserPut.class})
     private String phoneNumber;
+
     @JsonView({UserView.RegistrationPost.class, UserView.UserPut.class})
     private String cpf;
+
+    @NotBlank(groups = UserView.ImagePut.class)
     @JsonView(UserView.ImagePut.class)
     private String imageUrl;
 
