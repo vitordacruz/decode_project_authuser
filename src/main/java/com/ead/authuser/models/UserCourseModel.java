74d15e9c1e3d8dc5463521eb6b062c15c4b1d643
Vitor <vitordacruz@gmail.com>
2024-07-02 16:33:11 -0300

    Adicionado regra ao deletar um UserModel

--- src/main/java/com/ead/authuser/models/UserCourseModel.java
+++ src/main/java/com/ead/authuser/models/UserCourseModel.java
@@ -1,15 +1,14 @@
 package com.ead.authuser.models;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.persistence.*;
 import java.io.Serializable;
 import java.util.UUID;
 
-@Data
+@Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
