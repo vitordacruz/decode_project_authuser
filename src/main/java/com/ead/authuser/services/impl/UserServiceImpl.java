d12ab07eaecf4535616341655cf8066a7cd69f01
Vitor <vitordacruz@gmail.com>
2024-08-01 22:42:09 -0300

    Adicionado deleção entre microservices

--- src/main/java/com/ead/authuser/services/impl/UserServiceImpl.java
+++ src/main/java/com/ead/authuser/services/impl/UserServiceImpl.java
@@ -1,10 +1,12 @@
 package com.ead.authuser.services.impl;
 
+import com.ead.authuser.clients.CourseClient;
 import com.ead.authuser.models.UserCourseModel;
 import com.ead.authuser.models.UserModel;
 import com.ead.authuser.repositories.UserCourseRepository;
 import com.ead.authuser.repositories.UserRepository;
 import com.ead.authuser.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
@@ -38,14 +40,22 @@ public class UserServiceImpl implements UserService {
         return userRepository.findById(userId);
     }
 
+    @Autowired
+    private CourseClient courseClient;
+
     @Transactional
     @Override
     public void delete(UserModel user) {
+        boolean deleteUserCourseInCourse = false;
         List<UserCourseModel> userCourseModelList = userCourseRepository.findAllUserCourseIntoUser(user.getUserID());
         if (!userCourseModelList.isEmpty()) {
             userCourseRepository.deleteAll(userCourseModelList);
+            deleteUserCourseInCourse = true;
         }
         userRepository.delete(user);
+        if (deleteUserCourseInCourse) {
+            courseClient.deleteUserInCourse(user.getUserID());
+        }
     }
 
     @Override
